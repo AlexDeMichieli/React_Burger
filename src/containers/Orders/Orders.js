b9580d3433cc95b40ffb70c9b74923b51cd2b0d1
Alexander De Michieli <ademichieli@squarespace.com>
2020-04-08 21:18:16 -0700

    saving

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Orders = (props) => {
+
+    return(
+        <div>
+        {/* <p>Ingredients: Salad(1)</p>
+        <p>Price : <strong>USD 4.5</strong></p> */}
+        </div>
+    )
+}
+
+export default Orders
