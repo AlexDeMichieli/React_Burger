7cafac985e9c8a75721377b6a887dbfbe9ffc9fc
Alexander De Michieli <ademichieli@squarespace.com>
2020-04-20 22:10:04 -0700

    outputted orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,86 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
-import axios from '../../axios-orders'
+import { makeStyles } from '@material-ui/core/styles';
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      padding: "20px",
-    },
-    margin: {
-    margin: '30px 0'
-    },
-  }));
-
-const Order = (props) => {
-    const classes = useStyles();
-    const [order, setOrder]= useState(null)
-    const [isLoading, setIsLoading] = useState(false);
+  root: {
+    padding: "20px",
+  },
+  margin: {
+  margin: '30px 0'
+  },
+}));
 
 
-    useEffect(()=> {
-      async function getData(){
-        setIsLoading(true);
-        const data = await axios.get('/orders.json')
-        .then(res =>{
-            let ing = []
-            let address = []
-            let keys = res.data
-            if (keys != null){
-              console.log('we got an order')
-                keys = Object.values(res.data)
-                for (let ingredients in Object.entries(keys))
-                ing.push(Object.values(keys)[ingredients].ingredients)
-              setOrder(ing)
-            }else{
-              console.log('database empty')
-            }
-          setIsLoading(false);
-      })
-    }
-    getData()
-    }, [])
+const order = ( props ) => {
+  const classes = useStyles();
 
-    console.log('ORDER NULL', order=== null)
-
-  const test = () => {
-    let ingredients
-    if (order) {
-      ingredients = Object.values(order).map(item => {
-         let ing = Object.entries(item).map(el => {
-          return (
-          <p key={el}>{el[0]} {el[1]}</p>
-          )
-      });
-      return ing
-    })
-    } else {
-        return ingredients = (<p>no ingredients</p> )
-    } 
-    return ingredients
-  }
+  const ingredients = Object.entries(props.ingredients).map(item => {
+      return (<p key={item}>{item[0]}</p> )
+  })
 
 
-    return(
-        <Container maxWidth="sm" >
-          
-           {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <Container maxWidth="sm" >
-            <Paper elevation={2} className = {[classes.root, classes.margin].join(" ")}>
-              <p>Ingredients:</p>
-              {test()}
-              <p>Price : <strong>USD 4.5</strong></p>
-            </Paper>
-        </Container>
-       
-      )}
+    return (
+    <Container maxWidth="sm" >
+       <Paper elevation={2} className = {[classes.root, classes.margin].join(" ")}>
+         <h3>Ingredients:</h3>
+         {ingredients}
+         <span>Price : <strong>USD 4.5</strong></span>
+       </Paper>
+   </Container>
+    );
+};
 
-
-        </Container>
-    )
-}
-
-export default Order
+export default order;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,108 +1,44 @@
 import Order from '../../components/Order/Order'
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
 import axios from '../../axios-orders'
 
-// import React, { useState, useEffect } from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Paper from '@material-ui/core/Paper';
-// import Container from '@material-ui/core/Container';
-// import axios from '../../axios-orders'
+const Orders = (props) => {
+   
+    const [order, setOrder]= useState('')
+    const [isLoading, setIsLoading] = useState(false);
 
-// const Orders = (props) => {
+    useEffect(()=> {
+        axios.get('/orders.json')
+            .then(res => {
+                const myOrders = []
+                let keys = Object.values(res.data)
+                
+                for (let ingredients in Object.entries(keys)){
+                    myOrders.push(Object.values(keys)[ingredients].ingredients)
+                }
 
+                setOrder(myOrders);
+                setIsLoading(false)
+            })
 
-//     const [order, setOrder]= useState(null)
-//     const [isLoading, setIsLoading] = useState(false);
-
-
-//     useEffect(()=> {
-//       async function getData(){
-//         setIsLoading(true);
-//         const data = await axios.get('/orders.json')
-//         .then(res =>{
-//             let ing = []
-//             let address = []
-//             let keys = res.data
-//             if (keys != null){
-//               console.log('we got an order')
-//                 keys = Object.values(res.data)
-//                 for (let ingredients in Object.entries(keys))
-//                 ing.push(Object.values(keys)[ingredients].ingredients)
-//               setOrder(ing)
-//             }else{
-//               console.log('database empty')
-//             }
-//           setIsLoading(false);
-//       })
-//     }
-//     getData()
-//     }, [])
-
-//     console.log('ORDER NULL', order=== null)
-
-//   const test = () => {
-//     let ingredients
-//     if (order) {
-//       ingredients = Object.values(order).map(item => {
-//          let ing = Object.entries(item).map(el => {
-//           return (
-//           <p key={el}>{el[0]} {el[1]}</p>
-//           )
-//       });
-//       return ing
-//     })
-//     } else {
-//         return ingredients = (<p>no ingredients</p> )
-//     } 
-//     return ingredients
-//   }
+            .catch(err => {
+                setIsLoading(false);
+            });
+    }, [])
     
-//     return(
-//         <div key = {order}>
-//             <Order ingredients={test()}/>
-//         </div>
-//     )
-// }
-
-// export default Orders
-
-
-
-class Orders extends Component {
-
-       
-        state = { 
-
-            ingredients : [],
-            loading: true,
-            test: [1,2,3]
-
-         }
-
-         componentDidMount(){
-             axios.get('/orders.json')
-             .then(res=>{
-                 let fetchedOrders =[]
-                 for(let key in res.data){
-                    fetchedOrders.push({ fetchedOrders: res.data[key], id: key})
-                 }
-                 this.setState({ingredients: fetchedOrders, loading: false})
-             })
-         }
-
-    render() { 
-        console.log(this.state.ingredients)
-
-        return ( 
+        return (
             <div>
-            
-            <Order></Order>    
-           
-       
-               
+                {Object.values(order).map((order, number) => (
+                    <Order 
+                        key={number}
+                        ingredients={order}
+                    />
+                ))}
             </div>
-         );
-    }
+        );
 }
- 
+
 export default Orders;
